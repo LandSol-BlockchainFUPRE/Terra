8496d77c17817e708e477dab67d4352b5512c421
USER <junep059@gmail.com>
2025-05-27 08:35:07 +0100

    Installed Lucid and added connect wallet functionality

--- terra-service-tsx/next.config.ts
+++ terra-service-tsx/next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/koios/:path*',
+        destination: 'https://preview.koios.rest/api/v1/:path*',
+      },
+    ];
+  },
+
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      topLevelAwait: true,
+      layers: true,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
