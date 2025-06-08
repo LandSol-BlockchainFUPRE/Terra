85787e0056c9796daa58db358b7e1d798c6f76e8
sirisenju <amrohore4real@gmail.com>
2025-06-08 15:06:50 +0100

    modified

--- terra-service-tsx/src/app/client.tsx
+++ terra-service-tsx/src/app/client.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-const Home = dynamic(() => import('@/app/home'), { ssr: false });
+const Home = dynamic(() => import('@/app/home/page'), { ssr: false });
 
 export default function Client() {
   return <Home />;
